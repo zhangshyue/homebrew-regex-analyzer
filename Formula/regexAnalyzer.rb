8feee4c8caadf2b6cd47fc0f5c976397f799f391
Shiyue Zhang <zhangs@carleton.edu>
2022-02-13 19:26:13 -0600

    update formula

--- Formula/regexAnalyzer.rb
+++ Formula/regexAnalyzer.rb
@@ -4,7 +4,7 @@
 class Regexanalyzer < Formula
   desc "A Regex Analyzer"
   homepage "https://github.com/ase-regex-comps/regex-library"
-  url "https://github.com/ase-regex-comps/regex-library/blob/homebrew/archive/regex-library-1.0.0.tar.gz"
+  url "https://assets.ctfassets.net/4kcufowvm8o9/5tbA5pkaSCp0yOl3RwPiTD/09440a90c172c20218bc6c9aa6e60362/regex-library-1.0.0.tar.gz"
   sha256 "25420f532eb0ef4e4219defe36f4e6c1d02ca306f4ac4ad4050c5577f79af014"
   license "MIT"
 
