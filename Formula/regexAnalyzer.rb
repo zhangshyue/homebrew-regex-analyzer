389ec08c940abbec5de263a52913d5d99ae4be48
Shiyue Zhang <zhangs@carleton.edu>
2022-02-13 19:13:21 -0600

    add formula

--- Formula/regexAnalyzer.rb
+++ Formula/regexAnalyzer.rb
@@ -4,7 +4,7 @@
 class RegexAnalyzer < Formula
   desc "A Regex Analyzer"
   homepage "https://github.com/ase-regex-comps/regex-library"
-  url "https://github.com/ase-regex-comps/regex-library/blob/homebrew/archive/regex-library-1.0.0.tar.gz", :using => GitHubPrivateRepositoryReleaseDownloadStrategy
+  url "https://github.com/ase-regex-comps/regex-library/blob/homebrew/archive/regex-library-1.0.0.tar.gz"
   sha256 "25420f532eb0ef4e4219defe36f4e6c1d02ca306f4ac4ad4050c5577f79af014"
   license "MIT"
 
