539539ce8c3d53cf43ccccd5602a6375113814f1
Shiyue Zhang <zhangs@carleton.edu>
2022-02-15 20:24:39 -0600

    update formula

--- Formula/regexAnalyzer.rb
+++ Formula/regexAnalyzer.rb
@@ -4,17 +4,15 @@
 class Regexanalyzer < Formula
   desc "A Regex Analyzer"
   homepage "https://github.com/ase-regex-comps/regex-library"
-  url "https://github.com/zhangshyue/regex-library/archive/refs/tags/v1.0.2.tar.gz"
-  sha256 "cf5cff2097da2001d616f9be984c5e57683f07d1c8b740563ad0fe3a605d2b5d"
+  url "https://github.com/zhangshyue/regex-library/archive/refs/tags/v1.0.4.tar.gz"
+  sha256 "98c3ca56f0426de8284a6d1e1d0889c28db73a5b99892a7dec89b12bbeb748bf"
   license "MIT"
 
   depends_on "protobuf"
   depends_on "python@3.8"
-  depends_on "Textualize/rich/rich"
 
   def install
-    # libexec.install Dir["*"]
-    # bin.write_exec_script (libexec/"regex-analyzer")
+    bin.install "Makefile"
     bin.install "regex-analyzer"
     bin.install "main.py"   
     bin.install "root_pb2.py"   
@@ -26,6 +24,7 @@ class Regexanalyzer < Formula
     bin.install Dir["./protobuf"]
     bin.install Dir["./security"]
     bin.install Dir["./understandability"]
+    system "make", "install"
     # libexec.install Dir["*"]
     # bin.install_symlink "#{libexec}/bin/regex-library"
     # bin.write_exec_script (libexec/"regex-analyzer")
